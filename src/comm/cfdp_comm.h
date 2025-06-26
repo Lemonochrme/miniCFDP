2b2002e991874d1cd81844c97fd6073e347cc19e
Robin Marin--Muller <marin-muller@insa-toulouse.fr>
2025-06-26 13:34:37 +0000

    Declared cfdp communication layer interfaces

---
+++ src/comm/cfdp_comm.h
@@ -0,0 +1,21 @@
+#ifndef CFDP_COMM_H
+#define CFDP_COMM_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+
+// Initialize the communication layer
+int cfdp_comm_init(uint32_t local_port);
+
+// Send a CFDP PDU to a destination CFDP entity, the dest_entity_id will be mapped to a network address via the MIB
+int cfdp_comm_send(uint32_t dest_entity_id, const int8_t *data, size_t len);
+
+
+// receive a CFDP PDU (non-blocking)
+int cfdp_comm_receive(uint8_t *buffer, size_t buf_len, uint32_t *out_src_entity_id);
+
+// shutdown the communication layer (close sockets, etc...)
+int cfdp_comm_close(void);
+
+#endif
